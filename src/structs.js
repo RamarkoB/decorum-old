d88abf1f1784cfdcfdf5c399dc4565ed01bb5d18
RamarkoB <ramarkob@uchicago.edu>
2023-01-03 12:30:04 -0600

    Added Delegate Count cards

---
+++ src/structs.js
@@ -0,0 +1,232 @@
+//Delegate Classes
+const Attendence = {
+    Present: 'present',
+    Voting: 'voting',
+    Absent: 'absent'
+};
+  
+class Delegate {
+    constructor(name, attendence) {
+        this.name = name;
+        this.attendence = (attendence === undefined) ? Attendence.Absent : attendence;
+        this.timesSpoken = 0;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getAttendence(){
+        return this.attendence;
+    }
+
+    getTimesSpoken(){
+        return this.timesSpoken
+    }
+
+    markPresent(){
+        this.attendence = Attendence.Present;
+    }
+
+    markVoting(){
+        this.attendence = Attendence.Voting;
+    }
+
+    markAbsent(){
+        this.attendence = Attendence.Absent;
+    }
+
+    addTimeSpoken(){
+        this.timesSpoken++;
+    }
+
+    removeTimeSpoken(){
+        this.timesSpoken--;
+    }
+}
+
+
+//Speaker Classes
+class Speaker {
+    constructor() {
+        this.delegate = null;
+        this.hasDel = false;
+        this.spoken = false;
+    }
+
+    addDelegate(del) {
+        this.delegate = del;
+        this.hasDel = true;
+        // this.delegate.addTimeSpoken();
+    }
+
+    removeDelegate() {
+        this.delegate.removeTimeSpoken();
+        this.delegate = null;
+    }
+
+    hasDelegate(){
+        return this.hasDel;
+    }
+
+    getDelegate(){
+        return this.delegate;
+    }
+
+    getName(){
+        if (this.hasDel) {
+            return this.delegate.getName();
+        } else {
+            return "None";
+        }
+    }
+
+    speak() {
+        this.spoken = true;
+    }
+
+    hasSpoken(){
+        return this.spoken;
+    }
+}
+
+class SpeakersList {
+    constructor(numSpeakers){
+        this.numSpeakers = numSpeakers;
+        let listSpeakers = [];
+        for (let i = 0; i < this.numSpeakers; i ++) {
+            listSpeakers.push(new Speaker());
+        }
+        this.listSpeakers = listSpeakers;
+        this.speakerNum = 0;
+    }
+
+    addDelegate(i, del) {
+        this.listSpeakers[i].addDelegate(del);
+    }
+
+    removeDelegate(i, del) {
+        this.listSpeakers[i].removeDelegate(del);
+    }
+
+    // getName(num) {
+    //     return this.listSpeakers[num].getName();
+    // }
+
+    hasNext(){
+        return (this.speakerNum < this.NumSpeakers);
+    }
+
+    nextSpeaker() {
+        this.listSpeakers[this.speakerNum].speak();
+        this.speakerNum++;
+        return this.listSpeakers[this.speakerNum];
+    }
+}
+
+
+//Timer Classes
+const Status = {
+    Inactive: 'inactive',
+    Active: 'active',
+    Paused: 'paused'
+}
+
+class Timer {
+    constructor() {
+        this.length = {min:0, sec:0};
+        this.offset = 0;
+        this.deadline = null;
+        this.interval = null;
+        this.status = Status.Inactive;
+    }
+
+    play(deadline) {
+        function timerPlay(timer) {
+            function update(){ timer.update(); }
+            return setInterval(update, 100);
+        }
+
+        if (deadline == undefined){
+            this.deadline = new Date(Date.parse(new Date()) + this.offset);        
+        } else {
+            this.deadline = deadline;
+        }
+
+        this.interval = timerPlay(this);
+        this.status = Status.Active;
+
+        return this.deadline;
+    }
+
+    pause(offset) {
+        clearInterval(this.interval);
+        this.deadline = null;
+        this.interval = null;
+        this.status = Status.Paused;
+
+        if (offset != undefined){
+            this.offset = offset;
+        }
+
+        return this.offset;
+    }
+
+    update() {
+        this.offset = this.deadline - new Date();
+        if (this.offset <= 0) {
+            clearInterval(this.interval);
+            this.offset = 0;
+            this.active = false;
+        }
+    }
+
+    set(min, sec){
+        this.length.min = min;
+        this.length.sec = sec;
+        this.offset = Number(this.length.min) * 60 * 1000 + Number(this.length.sec) * 1000;
+    }
+
+    reset() {
+        this.pause();
+        this.offset = Number(this.length.min) * 60 * 1000 + Number(this.length.sec) * 1000;
+        this.status = Status.Paused;
+    }
+
+    write() {
+        this.pause();
+        this.status = Status.Inactive;
+    }
+
+    getStatus() {
+        return this.status;
+    }
+
+    getLength() {
+        return this.length;
+    }
+
+    getDeadline() {
+        return this.deadline;
+    }
+
+    timeLeft() {
+        const t = this.offset;
+        const seconds = Math.floor( (t/1000) % 60 );
+        const minutes = Math.floor( (t/1000/60));
+        const hours = Math.floor( (t/(1000*60*60)) % 24 );
+        const days = Math.floor( t/(1000*60*60*24) );
+        return {'total':t, 'days':days, 'hours':hours, 'minutes':minutes, 'seconds':seconds};
+    }
+}
+
+//Page
+const Page = {
+    delegates: "delegates",
+    motions: "motions",
+    directives: "directives",
+    mod: "mod",
+    unmod: "unmod",
+}
+
+export {Attendence, Delegate, Speaker, SpeakersList, Status, Timer, Page};
